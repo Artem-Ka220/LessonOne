753f4626b789f42e4312e8f60f9daa93222ec91a
arvsokolov <marktwe@mail.ru>
2023-05-18 22:25:20 +0300

    Added the last task.

---
+++ LessonSix/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 7: Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает последнюю цифру
+//этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+Console.WriteLine("Enter any nomber: ");
+int yourNumber = Convert.ToInt32(Console.ReadLine());
+yourNumber = yourNumber % 10;
+Console.WriteLine(yourNumber);
